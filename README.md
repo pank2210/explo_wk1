da9f1f7824b88cb61f9b9cc4df6dabe6aa0f4449
pank2210 <pank2210@yahoo.com>
2016-02-14 02:26:49 -0600

    submission of wk1

---
+++ README.md
@@ -0,0 +1,17 @@
+# explo_wk1
+
+This repos contains all submission for Exploratory research course week1 assignments
+
+below are key files
+
+1. plot1.R - Read data and draw requried historgram in plot1.png
+2. plot2.R - Read data and draw given line plot in plot2.png
+3. plot3.R - Read data and draw given multi line plot in plot3.png
+4. plot4.R - Read data and draw given multiple graphs in single plot4.png
+
+below are 4 png file each corresponding to above R script. Each file is of size 480X480 pixel
+
+5. plot1.png
+6. plot2.png
+7. plot3.png
+8. plot4.png
---
+++ plot4.R
@@ -0,0 +1,129 @@
+#plot4.R
+# R script is part of week-1 assignemnt submission of exploratory data anlysis course. 
+# Script does following thing,
+# - Reads hard coded UCI electric metre reading data from UCI ML data set.
+# - does basic data cleansing and col rearrangements 
+# - plot requested graph. (multiple line graph with legends)
+
+plot4 <- function(){
+  df.data <- downloadAndReadFile()
+  
+  #filter on requried date before processing any further
+  df.data <- subset(df.data,grepl("^[12]\\/2\\/2007",df.data$Date))
+  
+  #transform 
+  # - character to numeric for col 3 onwards if they are upgraded by fread
+  # - create new field datetime based on 1st col date & 2nd col time 
+  df.data <- transform(df.data, 
+                      datetime = as.POSIXct(paste(Date,Time),format="%d/%m/%Y %H:%M:%S"),
+                      Global_active_power = as.numeric(Global_active_power),
+                      Global_reactive_power = as.numeric(Global_reactive_power),
+                      Voltage = as.numeric(Voltage),
+                      Global_intensity = as.numeric(Global_intensity),
+                      Sub_metering_1 = as.numeric(Sub_metering_1),
+                      Sub_metering_2 = as.numeric(Sub_metering_2),
+                      Sub_metering_3 = as.numeric(Sub_metering_3)
+                       )
+  
+  #df.data <- subset(df.data,datetime)
+  #remove Date and Time as they are now populated as datetime
+  df.data$Date = NULL
+  df.data$Time = NULL
+  
+  #save old par
+  old.par <- par()
+
+  #set png device with rquired specs
+  png(width = 480,height = 480, units = "px",filename = "plot4.png")
+  
+  #4 frames for graphs
+  par(mfrow=c(2,2))
+
+  #draw 1st graph
+  plot(df.data$Global_active_power ~ df.data$datetime,
+       #col="black",
+       ylab="Gobal Active Power (kilowatts)",
+       xlab="",
+       main="",
+       type="n")
+       #mfrow=c(1,1))
+  lines(df.data$Global_active_power ~ df.data$datetime)
+  
+  #draw 2nd graph
+  with(df.data,plot(Voltage ~ datetime,
+       #col="red",
+       #ylab="Voltage",
+       #xlab="datetime",
+       type="n",
+       main=""))
+  with(df.data,lines(Voltage ~ datetime))
+  
+  #draw 3rd graph
+  plot(df.data$Sub_metering_1 ~ df.data$datetime,
+            #col="red",
+            ylab="Energy sub metering",
+            xlab="",
+            main="",
+            type="n")
+  
+  #draw individual line for each line item of Sub meter
+  lines(df.data$Sub_metering_1 ~ df.data$datetime, col="black")
+  lines(df.data$Sub_metering_2 ~ df.data$datetime, col="red")
+  lines(df.data$Sub_metering_3 ~ df.data$datetime, col="blue")
+  
+  #set legend
+  legend("topright",legend=names(df.data)[5:7],
+         lty=c(1,1,1),
+         col=c("black","red","blue"),
+         cex=0.5,
+         lwd=2)
+  
+  #draw 4th graph
+  with(df.data, plot(Global_reactive_power ~ datetime,
+       #col="red",
+       #ylab="Voltage",
+       #xlab="datetime",
+       type="n",
+       main=""))
+  with(df.data,lines(Global_reactive_power ~ datetime))
+  
+  #reset device
+  dev.off()
+  
+  #restore old par
+  par(old.par)
+  
+  #df.data
+}
+
+#function 
+# - download's file in data directory (if not it creates one and then download the zip from URL)
+# - unzip donwloaded file into tmp1 directory with overwrite option.
+# - reads data from unziped file using fread auto mode 
+# - deletes unziped file
+# - returns data.
+downloadAndReadFile <- function(dfile = "household_power_consumption.zip"){
+  unzipDir <- "data/tmp1/" 
+  destFile <- paste("data/",dfile,sep="")
+  unzipFile <- paste(unzipDir,"household_power_consumption.txt",sep="")
+  
+  message(sprintf("File downloaded [%s]",destFile))
+  message(sprintf("unzipFile file [%s]",unzipFile))
+  
+  if(!dir.exists("data")){
+    message("creating data directory!")
+    dir.create("data")
+  }
+  
+  if(!file.exists(destFile)){
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+                  destfile = destFile,method="libcurl")
+  }
+  
+  unzip( destFile, exdir=unzipDir, overwrite=TRUE)
+  data <- fread(unzipFile,header=TRUE,sep=";",
+                colClasses = c("character","character",rep("numeric",7)))
+  unlink(unzipFile)
+  
+  data
+}
